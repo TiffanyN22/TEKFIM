ba8272b785e125e0c58105d601566de54448ea59
mfollosco <mvfollosco@gmail.com>
2023-02-18 15:41:07 -0800

    login and signup pages

---
+++ TEKFIM/LoginController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginController.swift
+//  TEKFIM
+//
+//  Created by Madeline Follosco on 2/18/23.
+//
+
+import UIKit
+
+class LoginController: NSObject {
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var loginButt: UIButton!
+    @IBOutlet weak var signUpButt: UIButton!
+    
+    }
