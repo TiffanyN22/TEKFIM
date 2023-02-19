8521fa921685b5dba8fc3df3ad6ccedc034120f1
Kara Barrese <karabarrese@gmail.com>
2023-02-19 08:50:31 -0800

    ready

--- TEKFIM/InputTextUIViewController.swift
+++ TEKFIM/InputTextUIViewController.swift
@@ -18,13 +18,13 @@ class InputTextUIViewController: UIViewController {
     @IBAction func nextBut(_ sender: UIButton){
         performSegue(withIdentifier: "goToNext", sender: self)
     }
-    override func prepare(for seque: UIStoryboardSegue, sender: Any?){
-        if segue.identifier=="goToNext"{
-            let destinationVC = segue.destination as? VotingViewController
-            if let address = addressInput.text{
-                destinationVC?.address = address
-            }
-        }
-    }
+//    override func prepare(for seque: UIStoryboardSegue, sender: Any?){
+//        if segue.identifier=="goToNext"{
+//            let destinationVC = segue.destination as? VotingViewController
+//            if let address = addressInput.text{
+//                destinationVC?.address = address
+//            }
+//        }
+//    }
 
 }
