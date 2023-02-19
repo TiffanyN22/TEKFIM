ce36868180fc4b7677c15154d388af091a190776
TiffanyN22 <tnguyen22@ndsj.org>
2023-02-19 04:50:24 -0800

    Table for CandidatesViewController
    Untested

--- TEKFIM/CanidatesViewController.swift
+++ TEKFIM/CanidatesViewController.swift
@@ -1,56 +1,35 @@
 //
-//  CanidatesViewController.swift
+//  VotingViewController.swift
 //  TEKFIM
 //
 //  Created by Kara on 2/18/23.
 //
-
+import GoogleMaps
 import UIKit
-import AVFoundation
+import GooglePlaces
 
+class CandidatesViewController: UIViewController {
+    
+    let electionId = "2000"
+    let apiKey = "AIzaSyBjl48j1CVf4T5O-uaPsNY9d_FFOzOsKwM"
+    let address = "1263 Pacific Ave. Kansas City KS"
 
-class Contest {
-    var name: String
-    var participants: [String]
+    @IBOutlet weak var candidatesTable: UITableView!
     
-    init(name: String, participants: [String]) {
-        self.name = name
-        self.participants = participants
-    }
-}
+    var electionDisplayInfo: [String] = []
+    var isElectionDisplayContest: [Bool] = [] //true for contest, false for candidate
 
-class CanidatesViewController: UIViewController, UITableViewDataSource{
-    
-    var contests: [Contest] = []
-    var apiKey = "AIzaSyBjl48j1CVf4T5O-uaPsNY9d_FFOzOsKwM"
-    var addressNum = 1263
-    var address1st = "Pacific"
-    var address2nd = "Ave"
-    var city1st = "Kansas"
-    var city2nd = "City"
-    var state = "KS"
-    var electionID = 2000
-    
-    @IBOutlet weak var tableView: UITableView!
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         var url = "https://www.googleapis.com/civicinfo/v2/voterinfo?key=AIzaSyBjl48j1CVf4T5O-uaPsNY9d_FFOzOsKwM&address=1263%20Pacific%20Ave.%20Kansas%20City%20KS&electionId=2000"
-        
-        getData(from: url)
-        let contest1 = Contest(name: "Contest 1", participants: ["Alice", "Bob", "Charlie"])
-        let contest2 = Contest(name: "Contest 2", participants: ["Dave", "Eve"])
-        contests = [contest1, contest2]
-        
-        tableView.dataSource = self
 
-//        candidateTable.delegate = self
-//        candidateTable.dataSource = self
-        // Do any additional setup after loading the view.
+        getData(from: url)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            self.candidatesTable.reloadData()
+        }
     }
- 
+
     private func getData(from url: String){
         let task = URLSession.shared.dataTask(with: URL(string: url)!, completionHandler: { data, response, error in
             guard let data = data, error == nil else{
@@ -77,184 +56,75 @@ class CanidatesViewController: UIViewController, UITableViewDataSource{
             let k = json.contests.count-1
             for i in 0...k{
                 print(json.contests[i].type)
+                self.electionDisplayInfo.append(json.contests[i].type)
+                self.isElectionDisplayContest.append(true)
+                
                 if(json.contests[i].candidates != nil){
                     let m = json.contests[i].candidates!.count - 1
-                        for j in 0...m {
-                            print(json.contests[i].candidates![j].name)
-                            let contest = Contest(name: json.contests[i].type, participants: ["Alice", "Bob", "Charlie"])
-                        }
+                    for j in 0...m {
+                        print(json.contests[i].candidates![j].name)
+                        self.electionDisplayInfo.append(json.contests[i].candidates![j].name)
+                        self.isElectionDisplayContest.append(false)
+                    }
                 }
-                
-//
             }
         })
-
         task.resume()
-
     }
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return contests.count
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return contests[section].participants.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
-        let participant = contests[indexPath.section].participants[indexPath.row]
-        cell.textLabel?.text = participant
-        cell.accessoryType = .disclosureIndicator
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        let headerView = UIView()
-        headerView.backgroundColor = .lightGray
-        let headerLabel = UILabel(frame: CGRect(x: 15, y: 0, width: tableView.frame.size.width - 15, height: 30))
-        headerLabel.text = contests[section].name
-        headerLabel.textColor = .black
-        headerView.addSubview(headerLabel)
-        return headerView
-    }
-    
-//    class MyTableViewController: UITableViewController {
-//        var candidates: [String] = [] // Populate this with json.contests[i].candidates
-//
-//        override func viewDidLoad() {
-//            super.viewDidLoad()
-//            tableView.dataSource = self
-//        }
-//
-//        override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//            return candidates.count
-//        }
-//
-//        override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//            let cell = tableView.dequeueReusableCell(withIdentifier: "CandidateCell", for: indexPath) as! CandidateTableViewCell
-//            let candidate = candidates[indexPath.row]
-//            cell.NameLabel.text = candidate
-//            return cell
-//        }
-//    }
     
     struct groupRunning: Codable{
         let runFor: String
         var name = [String]()
     }
-    
     struct MyResult: Codable{
         let election: Election
-        //let normalizedInput: NormalizedInput
-        //let pollingLocations: [PollingLocations]
         var contests = [Contests]()
-        //let state: [State]
     }
-    
-//
-//    struct Sources: Codable{
-//        let name: String
-//        let official: Bool
-//    }
-    
     struct Election: Codable{
         let id: String
         let name: String
         let electionDay: String
         let ocdDivisionId: String
     }
-    
-    /*
-    struct NormalizedInput: Codable{
-        let line1: String
-        let city: String
-        let state: String
-        let zip: String
-    }
-    
-    struct PollingLocations: Codable{
-        let address: Address
-        let notes: String
-        let pollingHours: String
-        let sources: [Sources]
-    }
-    
-    struct Address: Codable{
-        let line1: String
-        let city: String
-        let state: String
-        let zip: String
-    }
-    */
-    
     struct Contests: Codable {
         let type: String
-        //let office: String
-        //let level: [Level]
-        //let roles: [Roles]
-        //let district: District
-        //let sources: [Sources]
         let candidates: [Candidates]?
     }
-    /*
-    struct Level: Codable {
-        let lvl: String
-    }
-    
-    struct Roles: Codable {
-        let role: String
-    }
-    
-    struct District: Codable{
-        let name: String
-        let scope: String
-        let id: String
-    }
-
-    */
     struct Candidates: Codable {
         let name: String
         let party: String
     }
-    /*
-    struct Channels: Codable {
-        let type: String
-        let id: String
-    }
-    
-    struct State: Codable{
-        let name: String
-        let electionAdministrationBody: ElectionAdministrationBody
-        let local_jurisdiction: Local_Jurisdiction
-        let sources: [Sources]
-    }
-    
-    struct ElectionAdministrationBody: Codable{
-        let name: String
-        let electionInfoUrl: String
-        let electionRegistrationUrl: String
-        let electionRegistrationConfirmationUrl: String
-        let absenteeVotingInfoUrl: String
-        let votingLocationFinderUrl: String
-        let ballotInfoUrl: String
-        let correspondanceAddress: CorrespondanceAddress
-    }
-    
-    struct CorrespondanceAddress: Codable{
-        let line1: String
-        let city: String
-        let state: String
-        let zip: String
-    }
-    
-    struct Local_Jurisdiction: Codable{
-        let name: String
-        let electionAdministrationBody: ElectionAdministrationBody
-        let sources: [Sources]
-    }
-     */
+}
 
+//table
+extension CandidatesViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true) //deselect or unhghlight app, indexPath is position
+//
+//        let vc = storyboard?.instantiateViewController(withIdentifier: "candidateTableCell") as! BillViewController
+//        navigationController?.pushViewController(vc, animated: true)
+    }
+}
+
+extension CandidatesViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return electionDisplayInfo.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "candidateTableCell", for:indexPath)
+        cell.textLabel?.text = self.electionDisplayInfo[indexPath.row]
+        cell.textLabel?.textAlignment = .center
+        cell.textLabel?.textColor = UIColor(named: "TekfimNavy")
+        cell.textLabel?.numberOfLines = 0
+        
+        if(isElectionDisplayContest[indexPath.row]){
+            cell.backgroundColor = UIColor(named: "TekfimRed")
+        } else{
+            cell.backgroundColor = UIColor(named: "TekfimGray")
+        }
+        return cell
+    }
 }
 
 
