f4de1ee04ec232d5e38334db92d36956a69bc518
Fernando Rojas <Fernando.rojas0422@gmail.com>
2023-02-18 18:01:59 -0800

    profile

---
+++ TEKFIM/ProfileSummaryViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ProfileSummaryViewController.swift
+//  TEKFIM
+//
+//  Created by Fernando Rojas on 2/18/23.
+//
+
+import UIKit
+
+class ProfileSummaryViewController: UIViewController {
+    @IBOutlet weak var Name: UILabel!
+    
+    @IBOutlet weak var District: UILabel!
+    
+    @IBOutlet weak var DistrictName: UILabel!
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        let apiKey = "cgBFpkuWG24jDavEv8jI0oWg54XgDenXJHPQUYGi"
+
+                let cosponsorUrl = "https://api.congress.gov/v3/bill/117/hr/3076/cosponsors?api_key=\(apiKey)&format=json"
+
+                getCosponsordata(from: cosponsorUrl)
+
+
+                
+
+            }
+    }
+
+
