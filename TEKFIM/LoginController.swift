b54b351f424b090a77551245b894645ff1d9ccf1
mfollosco <mvfollosco@gmail.com>
2023-02-18 16:15:41 -0800

    login/sinup pages

---
+++ TEKFIM/LoginController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginController.swift
+//  TEKFIM
+//
+//  Created by Madeline Follosco on 2/18/23.
+//
+
+import UIKit
+
+class LoginController: NSObject {
+
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passField: UITextField!
+    @IBOutlet weak var loginButt: UIButton!
+    @IBOutlet weak var signUpButt: UIButton!
+}
---
+++ TEKFIM/SignUpController.swift
@@ -0,0 +1,16 @@
+//
+//  SignUpController.swift
+//  TEKFIM
+//
+//  Created by Madeline Follosco on 2/18/23.
+//
+
+import UIKit
+
+class SignUpController: NSObject {
+
+    @IBOutlet weak var signUpEmailField: UITextField!
+    @IBOutlet weak var signUpPassField: UITextField!
+    @IBOutlet weak var addressField: UITextField!
+    @IBOutlet weak var signUpInfoButt: UIButton!
+}
